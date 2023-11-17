8bae4ccf87674b4df7b737b2ab23ae5dc6b7077e
kenny1303 <arevalosarmientokenny@gmail.com>
2023-11-17 08:57:11 -0500

    actualizando diccionarios

--- src/Model/Aeropuertos.py
+++ src/Model/Aeropuertos.py
@@ -19,167 +19,197 @@ class Aeropuerto(db.Model):
 
 aeropuertos_nacionales = [
     {
-        "Nombre": "Antonio Roldán Betancourt",
-        "Teléfono": "6045208543",
-        "Dirección": "Antioquia, Colombia"
+        "Nombre": "El Edén",
+        "Teléfono": "67479400",
+        "Dirección": "Quindío / Colombia"
     },
     {
-        "Nombre": "Santiago Pérez Quiroz",
-        "Teléfono": "078852297",
-        "Dirección": "Arauca, Colombia"
+        "Nombre": "Palonegro",
+        "Teléfono": "(607) 6910140",
+        "Dirección": "Santander / Colombia"
     },
     {
-        "Nombre": "Yariguíes",
-        "Teléfono": "3175180631",
-        "Dirección": "Barrancabermeja, Colombia"
+        "Nombre": "Rafael Núñez",
+        "Teléfono": "(5) 6931351",
+        "Dirección": "Cartagena / Bolívar / Colombia"
     },
     {
-        "Nombre": "Gustavo Artunduaga",
-        "Teléfono": "84351495",
-        "Dirección": "Florencia, Colombia"
+        "Nombre": "Gustavo Rojas Pinilla",
+        "Teléfono": "+57 8 512 6112",
+        "Dirección": "San Andrés y Providencia / Colombia"
     },
     {
-        "Nombre": "Aeropuerto Perales",
-        "Teléfono": "6014251000",
-        "Dirección": "Ibagué, Colombia"
+        "Nombre": "Los Garzones",
+        "Teléfono": "(+57) 604 7911476",
+        "Dirección": "Montería / Córdoba / Colombia"
     },
     {
-        "Nombre": "Javier Noreña Valencia",
-        "Teléfono": "3102802054",
-        "Dirección": "La Macarena, Colombia"
+        "Nombre": "Almirante Padilla",
+        "Teléfono": "301 3445618",
+        "Dirección": "Riohacha / Colombia"
     },
     {
-        "Nombre": "La Nubia",
-        "Teléfono": "6068745451",
-        "Dirección": "Manizales, Colombia"
+        "Nombre": "Aeropuerto Internacional Ernesto Cortissoz (BAQ), Barranquilla, Colombia",
+        "Teléfono": "6053160900",
+        "Dirección": "Barranquilla, Colombia"
     },
     {
-        "Nombre": "Enrique Olaya Herrera",
-        "Teléfono": "6043656100",
+        "Nombre": "Aeropuerto Internacional El Dorado (BOG), Bogotá, Colombia",
+        "Teléfono": "6012662000",
+        "Dirección": "Bogotá, Colombia"
+    },
+    {
+        "Nombre": "Aeropuerto Internacional Alfonso Bonilla Aragón (CLO), Cali, Colombia",
+        "Teléfono": "6663026",
+        "Dirección": "Cali, Colombia"
+    },
+    {
+        "Nombre": "Aeropuerto Internacional Camilo Daza (CUC), Cúcuta, Colombia",
+        "Teléfono": "7433073",
+        "Dirección": "Cúcuta, Colombia"
+    },
+    {
+        "Nombre": "Aeropuerto Alfredo Vásquez Cobo (LET), Leticia, Colombia",
+        "Teléfono": "14251000",
+        "Dirección": "Leticia, Colombia"
+    },
+    {
+        "Nombre": "Aeropuerto Internacional José María Córdova (MDE), Medellín, Colombia",
+        "Teléfono": "604 402 5110",
         "Dirección": "Medellín, Colombia"
     },
     {
-        "Nombre": "Villagarzón",
-        "Teléfono": "3112546898",
-        "Dirección": "Villagarzón, Colombia"
+        "Nombre": "Aeropuerto Internacional Matecaña (PEI), Pereira, Colombia",
+        "Teléfono": "3148151",
+        "Dirección": "Pereira, Colombia"
     },
     {
-        "Nombre": "Benito Salas",
-        "Teléfono": "7887570",
-        "Dirección": "Neiva, Colombia"
+        "Nombre": "Aeropuerto Internacional Simón Bolívar (SMR), Santa Marta, Colombia",
+        "Teléfono": "54381360",
+        "Dirección": "Santa Marta, Colombia"
     },
     {
-        "Nombre": "Antonio Nariño",
+        "Nombre": "Aeropuerto Antonio Nariño (PSO), Pasto, Colombia",
         "Teléfono": "3167185787",
         "Dirección": "Pasto, Colombia"
-    },
-    {
-        "Nombre": "Guillermo León Valencia",
-        "Teléfono": "28231900",
-        "Dirección": "Popayán, Colombia"
-    },
-    {
-        "Nombre": "El Embrujo",
-        "Teléfono": "85148176",
-        "Dirección": "San Andrés, Colombia"
-    },
-    {
-        "Nombre": "Tres de Mayo",
-        "Teléfono": "3202921317",
-        "Dirección": "Putumayo, Colombia"
-    },
-    {
-        "Nombre": "Morelia",
-        "Teléfono": "",
-        "Dirección": "Puerto Gaitán, Colombia"
-    },
-    {
-        "Nombre": "César Gaviria Trujillo",
-        "Teléfono": "85656069",
-        "Dirección": "Guainía, Colombia"
-    },
-    {
-        "Nombre": "Germán Olano",
-        "Teléfono": "3102880476",
-        "Dirección": "Puerto Carreño, Colombia"
-    },
-    {
-        "Nombre": "El Caraño",
-        "Teléfono": "46711537",
-        "Dirección": "Quibdó, Colombia"
-    },
-    {
-        "Nombre": "Jorge Enrique González",
-        "Teléfono": "3228546866",
-        "Dirección": "Guaviare, Colombia"
-    },
-    {
-        "Nombre": "Las Brujas",
-        "Teléfono": "52499735",
-        "Dirección": "Sincelejo, Colombia"
-    },
-    {
-        "Nombre": "La Florida",
-        "Teléfono": "2272598",
-        "Dirección": "Tumaco, Colombia"
-    },
-    {
-        "Nombre": "Puerto Bolívar",
-        "Teléfono": "3215431281",
-        "Dirección": "La Guajira, Colombia"
-    },
-    {
-        "Nombre": "Alfonso López Pumarejo",
-        "Teléfono": "6055582323",
-        "Dirección": "Valledupar, Colombia"
-    },
-    {
-        "Nombre": "Vanguardia",
-        "Teléfono": "3217622520",
-        "Dirección": "Villavicencio,Colombia"
-    },
-    {
-        "Nombre": "El Alcaraván",
-        "Teléfono": "86358352",
-        "Dirección": "Yopal, Colombia"
     }
 ]
 aeropuertos_extranjeros = [
     {
-        "Nombre": "Aeropuerto Internacional John F. Kennedy (JFK)",
-        "Dirección": "Nueva York, Estados Unidos"
-    },
-    {
-        "Nombre": "Aeropuerto Heathrow de Londres (LHR)",
-        "Dirección": "Reino Unido, Londres"
-    },
-    {
-        "Nombre": "Aeropuerto Internacional de Dubái (DXB)",
+        "Nombre": "Aeropuerto Internacional de Dubái (DXB), Dubái, Emiratos Árabes Unidos",
         "Dirección": "Dubái, Emiratos Árabes Unidos"
     },
     {
-        "Nombre": "Aeropuerto Internacional de Narita (NRT)",
-        "Dirección": " Tokio, Japón"
-    },
-    {
-        "Nombre": "Aeropuerto Internacional de Hamad (DOH)",
+        "Nombre": "Aeropuerto Internacional de Hamad (DOH), Doha, Catar",
         "Dirección": "Doha, Catar"
     },
     {
-        "Nombre": "Aeropuerto Changi de Singapur (SIN)",
+        "Nombre": "Aeropuerto Changi de Singapur (SIN), Singapur, Singapur",
         "Dirección": "Singapur, Singapur"
     },
     {
-        "Nombre": "Aeropuerto Suvarnabhumi de Bangkok (BKK)",
+        "Nombre": "Aeropuerto Suvarnabhumi de Bangkok (BKK), Bangkok, Tailandia",
         "Dirección": "Bangkok, Tailandia"
     },
     {
-        "Nombre": "Aeropuerto Kingsford Smith de Sídney (SYD)",
+        "Nombre": "Aeropuerto Internacional John F. Kennedy (JFK), Nueva York, EE. UU.",
+        "Dirección": "Nueva York, EE. UU."
+    },
+    {
+        "Nombre": "Aeropuerto Heathrow de Londres (LHR), Londres, Reino Unido",
+        "Dirección": "Londres, Reino Unido"
+    },
+    {
+        "Nombre": "Aeropuerto Internacional de Narita (NRT), Tokio, Japón",
+        "Dirección": " Tokio, Japón"
+    },
+    {
+        "Nombre": "Aeropuerto Kingsford Smith de Sídney (SYD), Sídney, Australia",
         "Dirección": "Sídney, Australia"
     },
     {
-        "Nombre": "Aeropuerto Internacional de Pekín-Capital (PEK)",
+        "Nombre": "Aeropuerto Internacional de Pekín-Capital (PEK), Pekín, China",
         "Dirección": "Pekín, China"
+    },
+    {
+        "Nombre": "Aeropuerto Internacional de Miami (MIA), Miami, EE. UU.",
+        "Dirección": "Miami, EE. UU."
+    },
+    {
+        "Nombre": "Aeropuerto Adolfo Suárez Madrid-Barajas (MAD), Madrid, España",
+        "Dirección": "Madrid, España"
+    },
+    {
+        "Nombre": "Aeropuerto Internacional de Guarulhos (GRU), Sao Paulo, Brasil",
+        "Dirección": "Sao Paulo, Brasil"
+    },
+    {
+        "Nombre": "Aeropuerto Internacional de la Ciudad de México (MEX), Ciudad de México, México",
+        "Dirección": "Ciudad de México, México"
+    },
+    {
+        "Nombre": "Aeropuerto Charles de Gaulle (CDG), París, Francia",
+        "Dirección": "París, Francia"
+    },
+    {
+        "Nombre": "Aeropuerto Internacional Ministro Pistarini (EZE), Buenos Aires, Argentina",
+        "Dirección": "Buenos Aires, Argentina"
+    },
+    {
+        "Nombre": "Aeropuerto Internacional Jorge Chávez (LIM), Lima, Perú",
+        "Dirección": "Lima, Perú"
+    },
+    {
+        "Nombre": "Aeropuerto de Roma-Fiumicino (FCO), Roma, Italia",
+        "Dirección": "Roma, Italia"
+    },
+    {
+        "Nombre": "Aeropuerto Internacional Toronto Pearson (YYZ), Toronto, Canadá",
+        "Dirección": "Toronto, Canadá"
+    },
+    {
+        "Nombre": "Aeropuerto de Brisbane (BNE), Brisbane, Australia",
+        "Dirección": "Melbourne, Australia"
+    },
+    {
+        "Nombre": "Aeropuerto Internacional de Los Ángeles (LAX), Los Ángeles, EE. UU.",
+        "Dirección": "Los Ángeles, EE. UU."
+    },
+    {
+        "Nombre": "Aeropuerto Internacional Indira Gandhi (DEL), Nueva Delhi, India",
+        "Dirección": "Nueva Delhi, India"
+    },
+    {
+        "Nombre": "Aeropuerto Internacional Hartsfield-Jackson (ATL), Atlanta, EE. UU.",
+        "Dirección": "Atlanta, EE. UU."
+    },
+    {
+        "Nombre": "Aeropuerto Internacional Louis Armstrong (MSY), Nueva Orleans, EE. UU.",
+        "Dirección": "Nueva Orleans, EE. UU."
+    },
+    {
+        "Nombre": "Aeropuerto Internacional Simón Bolívar (CCS), Caracas, Venezuela",
+        "Dirección": "Caracas, Venezuela"
+    },
+    {
+        "Nombre": "Aeropuerto Internacional Luis Muñoz Marín (SJU), San Juan, Puerto Rico",
+        "Dirección": "San Juan, Puerto Rico"
+    },
+    {
+        "Nombre": "Aeropuerto Internacional José Martí (HAV), La Habana, Cuba",
+        "Dirección": "La Habana, Cuba"
+    },
+    {
+        "Nombre": "Aeropuerto Humberto Delgado (LIS), Lisboa, Portugal",
+        "Dirección": "Lisboa, Portugal"
+    },
+    {
+        "Nombre": "Aeropuerto Internacional de Sheremétievo (SVO), Moscú, Rusia",
+        "Dirección": "Moscú, Rusia"
+    },
+    {
+        "Nombre": "Aeropuerto Internacional de San Francisco (SFO), San Francisco, EE. UU.",
+        "Dirección": "San Francisco, EE. UU."
     }
 ]
 
