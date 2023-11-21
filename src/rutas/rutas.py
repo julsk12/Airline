c08d3dc9fdb6b6bb89e9ecc5401ee9b2ddbf9135
JorgeDuran12 <duranolivaresjorgeluis1@gmail.com>
2023-11-21 16:22:38 -0500

    mm

--- src/rutas/rutas.py
+++ src/rutas/rutas.py
@@ -7,3 +7,13 @@ routes_home = Blueprint("routes_home", __name__)
 @routes_home.route('/indexvuelos', methods=['GET'] )
 def indexvuelos():
     return render_template('/public/vuelos.html')
+
+
+@routes_home.route('/indexlogin', methods=['GET'] )
+def indexlogin():
+    return render_template('/public/login.html')
+
+
+@routes_home.route('/indexregistro', methods=['GET'] )
+def indexregistro():
+    return render_template('/public/registro.html')
