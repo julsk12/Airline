2c81d7bcd004e54d7a58c5809473a5bf5aea2674
julsk12 <jpsierra316@misena.edu.co>
2023-11-21 20:23:04 -0500

    algo

--- src/rutas/rutas.py
+++ src/rutas/rutas.py
@@ -1,6 +1,6 @@
 from config.db import db, app, ma
 from flask import Blueprint, Flask,  redirect, request, jsonify, json, session, render_template
-from common.Toke import *
+# from common.Toke import *
 
 routes_home = Blueprint("routes_home", __name__)
 
