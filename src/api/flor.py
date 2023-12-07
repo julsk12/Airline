85d39dd29811ffc9a7bca73e1d4cbe08862c89cc
benedettysw <benedetty321@gmail.com>
2023-12-07 08:53:16 -0500

    ..

---
+++ src/api/flor.py
@@ -0,0 +1,20 @@
+import turtle
+import colorsys
+
+turtle.speed(0)
+turtle.bgcolor("black")
+h = 10
+
+for i in range(16):
+    for j in range(18):
+        c = colorsys.hsv_to_rgb(h, 1, 1)  # Use maximum saturation and value
+        turtle.color(c[0], c[1], c[2])  # Unpack the tuple
+        h += 0.005
+        turtle.rt(90)
+        turtle.circle(150 - j * 6, 90)
+        turtle.lt(90)
+        turtle.circle(150 - j * 6, 90)
+        turtle.rt(180)
+        turtle.circle(40, 24)
+
+turtle.done()
